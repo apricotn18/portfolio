43889050050b9a15b0d07833222e99ce35e359e6
apricotn <56545404+apricotn18@users.noreply.github.com>
2025-03-28 16:42:19 +0900

    Update skill.tsx

--- pages/component/skill/skill.tsx
+++ pages/component/skill/skill.tsx
@@ -49,13 +49,13 @@ export default function Page() {
 						<p>
 							HTML / CSSは疑似要素やアニメーション、セマンティックなマークアップ（SEO）が可能です。デザインツールも実務レベルで使用できます。<br />
 							JavaScriptはインタラクションの実装、APIの取得・操作、非同期処理など実務レベルで使用できます。<br />
-							TypeScriptは単純な型指定ができます。<br />
+							TypeScriptは単純な型指定が可能です。<br />
 							React (Next.js) / Node.js / Justは公式チュートリアルやリファレンス本を読了し、ポートフォリオにある簡単なWebアプリを個人開発しました。
 						</p>
 						<ol className={styles.level}>
 							<li>3.  実務レベル</li>
 							<li>2.  実務で軽く使用あり、習熟が必要</li>
-							<li>1.  勉強中</li>
+							<li>1.  勉強中、実務で使用経験なし</li>
 						</ol>
 					</div>
 				</div>
