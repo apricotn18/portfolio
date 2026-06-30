cd5f2c66e71ef20ec6a54b0042018cf094256c4b
apricotn18 <apricotn18@gmail.com>
2026-06-30 10:23:00 +0900

    refactor(work): 波アニメーションをSVGからbackground-image方式に変更

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss';
 export default function Work() {
 	return (
 		<section id="work" className={styles.work}>
+			<div className={styles.waveWrapper} />
 			<h2 className={styles.title}>WORK</h2>
 		</section>
 	);
